29efc93a8e0d265972859d586cc8ac4afeeaa547
capmahin <mahinulhuque1996@gmail.com>
2022-07-25 04:30:49 +0600

    add back-end and install all needed

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k6znvtm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("dui takar pepsi sakib bhai sexy");
+});
+
+app.listen(port, () => {
+  console.log(`Sakib Bhai  listening on port ${port}`);
+});
