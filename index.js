75f420a80974db84c60de8a1c054d20b3c727917
capmahin <mahinulhuque1996@gmail.com>
2022-08-01 01:48:49 +0600

    add rooms and user in socket

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const server = require("http").createServer(app);
+
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 const { nanoid } = require("nanoid");
-const io = require("socket.io")(server, {
+const io = require("socket.io")(app, {
   cors: {
     origin: "+",
   },
@@ -27,6 +27,45 @@ const rooms = [];
 
 io.on("connection", (socket) => {
   socket.emit("me", socket.id);
+  users.push(socket.id);
+  socket.broadcast.emit("updateUsers", users);
+
+  socket.on("disconnect", () => {
+    users = users.filter((users) => user !== socket.id);
+    socket.broadcast.emit("updateUsers", users);
+    socket.disconnect();
+  });
+
+  socket.emit("getAllUsers", users);
+
+  //Rooms
+
+  socket.on("create_room", () => {
+    const room = {
+      id: nanoid(7),
+      chat: [],
+    };
+    socket.join(room);
+    socket.emit("get_room", room);
+    rooms.push(room);
+    socket.broadcast.emit("updateRooms", rooms);
+  });
+
+  socket.on("join_room", (room) => {
+    socket.join(room.id);
+  });
+  socket.broadcast.emit("updateRooms", rooms);
+
+  socket.on("message", (payload) => {
+    rooms.map((room) => {
+      if (room.id === payload.room) {
+        singleChat = { message: payload.message, writer: payload.socketId };
+        room.chat.push(singleChat);
+      }
+    });
+
+    io.to(payload.room).emit("chat", payload);
+  });
 });
 
 async function run() {
