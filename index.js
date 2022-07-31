ebd735a41871906a68f1df3fd4c7803e2aa3aa23
capmahin <mahinulhuque1996@gmail.com>
2022-08-01 01:23:40 +0600

    add socket io and nanoid

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const server = require("http").createServer(app);
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+const { nanoid } = require("nanoid");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "+",
+  },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -15,43 +22,36 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+const users = [];
+const rooms = [];
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+});
+
 async function run() {
   try {
-    await client.connect()
+    await client.connect();
 
-    const reviewsCollection = client.db('LanguageFixer').collection('userReview')
+    const reviewsCollection = client
+      .db("LanguageFixer")
+      .collection("userReview");
 
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewsCollection.insertOne(review);
+      res.send(result);
+    });
 
-    app.post('/reviews', async (req, res) => {
-      const review = req.body
-      const result = await reviewsCollection.insertOne(review)
-      res.send(result)
-    })
-
-
-    app.get('/review', async (req, res) => {
-      const reviews = await reviewsCollection.find().toArray()
-      res.send(reviews)
-    })
-
-
-
-
-
-
+    app.get("/review", async (req, res) => {
+      const reviews = await reviewsCollection.find().toArray();
+      res.send(reviews);
+    });
   } finally {
-
-
-
   }
 }
 
-
 run().catch(console.dir);
-
-
-
-
 
 app.get("/", (req, res) => {
   res.send("dui takar pepsi sakib bhai sexy");
