25dbddcaacfe7648aab009af659a738bcaae1028
capmahin <mahinulhuque1996@gmail.com>
2022-08-02 18:35:08 +0600

    finish chat application back end

--- index.js
+++ index.js
@@ -9,21 +9,61 @@ const server = http.createServer(app);
 const io = socketio(server);
 const PORT = process.env.PORT || 5000;
 const port = process.env.port || 5001;
-
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 const router = require("./router");
-
-io.on("connection", (socket) => {
-  console.log("we have a new connection");
-  socket.on("disconnect", () => {
-    console.log("User had left");
-  });
-});
 app.use(router);
-
-server.listen(PORT, () => console.log(`Server has started on ${PORT} `));
-
 app.use(cors());
 app.use(express.json());
+
+io.on("connect", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to room ${user.room}.`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left.`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+});
+
+server.listen(PORT, () => console.log(`Server has started on ${PORT} `));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.akik6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
