7ed5d26dc6d72a912d8bfa7b6cb1d1164fe16677
Shahria-Turag <shahriarturag.st@gmail.com>
2022-08-10 00:54:37 +0600

    test

--- index.js
+++ index.js
@@ -14,71 +14,68 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "https://language-fixer.vercel.app/",
-    methods: ["GET", "POST"],
-  },
+    cors: {
+        origin: "https://language-fixer.vercel.app/",
+        methods: ["GET", "POST"],
+    },
 });
 
 io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+    console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+    });
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
 });
 
 server.listen(5001, () => {
-  console.log("SERVER RUNNING");
+    console.log("SERVER RUNNING");
 });
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.akik6.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x3cu1xp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
 });
 
 async function run() {
-  try {
-    await client.connect();
+    try {
+        await client.connect();
 
-    const reviewsCollection = client
-      .db("LanguageFixer")
-      .collection("userReview");
+        const reviewsCollection = client
+            .db("LanguageFixer")
+            .collection("userReview");
 
-    app.post("/reviews", async (req, res) => {
-      const review = req.body;
-      const result = await reviewsCollection.insertOne(review);
-      res.send(result);
-    });
+        app.post("/reviews", async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.send(result);
+        });
 
-    app.get("/review", async (req, res) => {
-      const reviews = await reviewsCollection.find().toArray();
-      res.send(reviews);
-    });
-  } finally {
-  }
+        app.get("/review", async (req, res) => {
+            const reviews = await reviewsCollection.find().toArray();
+            res.send(reviews);
+        });
+    } finally {
+    }
 }
 
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("dui takar pepsi sakib bhai sexy");
+    res.send("dui takar pepsi sakib bhai sexy");
 });
 
-// app.listen(port, () => {
-//
-// });
 app.listen(port, () => {
-  console.log(`Sakib Bhai  listening on port ${port}`);
+    console.log(`Sakib Bhai is listening on port ${port}`);
 });
