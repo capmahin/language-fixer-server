431e7b240bc6f5b2c41f726ec1b27e4eff521ea6
capmahin <mahinulhuque1996@gmail.com>
2022-08-16 17:03:54 +0600

    new one

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ var server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
+    
     methods: ["GET", "POST"],
   },
 });
@@ -30,6 +31,23 @@ io.on("connection", (socket) => {
 
   socket.on("newMessage", ({ newMessage, room }) => {
     io.in(room).emit("getLatestMessage", newMessage);
+  });
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
   });
 });
 
