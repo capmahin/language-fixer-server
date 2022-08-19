eb0118db2f8c895f1cc1a0f797b1693bee7682fa
Shahria-Turag <shahriarturag.st@gmail.com>
2022-08-19 19:11:34 +0600

    test

--- index.js
+++ index.js
@@ -15,44 +15,43 @@ app.use(express.json());
 
 app.use(cors());
 
-
 var server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "https://language-fixer.vercel.app",
+    cors: {
+        origin: "https://language-fixer.vercel.app",
 
-    methods: ["GET", "POST"],
-  },
+        methods: ["GET", "POST"],
+    },
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+    console.log(socket.id);
 
-  socket.on("joinRoom", (room) => {
-    socket.join(room);
-  });
-
-  socket.on("newMessage", ({ newMessage, room }) => {
-    io.in(room).emit("getLatestMessage", newMessage);
-  });
-  socket.emit("me", socket.id);
-
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callEnded");
-  });
-
-  socket.on("callUser", (data) => {
-    io.to(data.userToCall).emit("callUser", {
-      signal: data.signalData,
-      from: data.from,
-      name: data.name,
+    socket.on("joinRoom", (room) => {
+        socket.join(room);
     });
-  });
 
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal);
-  });
+    socket.on("newMessage", ({ newMessage, room }) => {
+        io.in(room).emit("getLatestMessage", newMessage);
+    });
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded");
+    });
+
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit("callUser", {
+            signal: data.signalData,
+            from: data.from,
+            name: data.name,
+        });
+    });
+
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal);
+    });
 });
 
 // server.listen(5001, () => {
@@ -65,106 +64,110 @@ io.on("connection", (socket) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x3cu1xp.mongodb.net`;
 
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
 });
 
 function verifyJWT(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  if (!authHeader) {
-    return res.status(401).send({ message: "Unauthorized access" });
-  }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-    if (err) {
-      return res.status(401).send({ message: "Forbidden access" });
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized access" });
     }
-    req.decoded = decoded;
-    next();
-  });
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: "Forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
 }
 
 async function run() {
-  try {
-    await client.connect();
+    try {
+        await client.connect();
 
-    const reviewsCollection = client
-      .db("LanguageFixer")
-      .collection("userReview");
-    const userCollection = client.db("LanguageFixer").collection("users");
+        const reviewsCollection = client
+            .db("LanguageFixer")
+            .collection("userReview");
+        const userCollection = client.db("LanguageFixer").collection("users");
 
-    const blogsCollection = client.db("LanguageFixer").collection("blogs");
+        const blogsCollection = client.db("LanguageFixer").collection("blogs");
 
-    app.get("/user", async (req, res) => {
-      const users = await userCollection.find().toArray();
-      res.send(users);
-    });
+        app.get("/user", async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
 
-    app.put("/user/admin/:email", async (req, res) => {
-      const email = req.params.email;
-      const filter = { email: email };
-      const updateDoc = {
-        $set: { role: "admin" },
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
+        app.put("/user/admin/:email", async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: "admin" },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
 
-      res.send({ result });
-    });
+            res.send({ result });
+        });
 
-    app.put("/user/:email", async (req, res) => {
-      const email = req.params.email;
-      const user = req.body;
-      const filter = { email: email };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: user,
-      };
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
 
-      const result = await userCollection.updateOne(filter, updateDoc, options);
+            const result = await userCollection.updateOne(
+                filter,
+                updateDoc,
+                options
+            );
 
-      const token = jwt.sign(
-        { email: email },
-        process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "1h" }
-      );
-      res.send({ result, token });
-    });
+            const token = jwt.sign(
+                { email: email },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: "1h" }
+            );
+            res.send({ result, token });
+        });
 
-    app.post("/reviews", async (req, res) => {
-      const review = req.body;
-      const result = await reviewsCollection.insertOne(review);
-      res.send(result);
-    });
+        app.post("/reviews", async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.send(result);
+        });
 
-    app.get("/review", async (req, res) => {
-      const reviews = await reviewsCollection.find().toArray();
-      res.send(reviews);
-    });
+        app.get("/review", async (req, res) => {
+            const reviews = await reviewsCollection.find().toArray();
+            res.send(reviews);
+        });
 
-    app.get("/blogs", async (req, res) => {
-      const blogs = await blogsCollection.find().toArray();
-      res.send(blogs);
-    });
-    app.get("/blogs/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const blog = await blogsCollection.findOne(query);
-      res.send(blog);
-    });
-  } finally {
-  }
+        app.get("/blogs", async (req, res) => {
+            const blogs = await blogsCollection.find().toArray();
+            res.send(blogs);
+        });
+        app.get("/blogs/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const blog = await blogsCollection.findOne(query);
+            res.send(blog);
+        });
+    } finally {
+    }
 }
 
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("dui takar pepsi sakib bhai sexy");
+    res.send("dui takar pepsi sakib bhai sexy");
 });
 
 // app.listen(port, () => {
 //
 // });
 server.listen(port, () => {
-  console.log(`Sakib Bhai  listening on port ${port}`);
+    console.log(`Sakib Bhai  listening on port ${port}`);
 });
